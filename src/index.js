5243e007d28f992e53c80d6d10d928e113f8f1e5
arjun.betageri <arjun.betageri@live.com>
2025-02-26 12:25:10 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App"; // Ensure correct casing
+import App from "./app"; // Ensure correct casing
 
 ReactDOM.render(
   <React.StrictMode>
